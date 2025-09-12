df93c063497d5402625c1a926063ce3ed6de0caf
Perry Huang <perryhuang2015@gmail.com>
2025-09-12 15:04:34 -0400

    feat: add transformation lesson

--- main.cpp
+++ main.cpp
@@ -8,12 +8,19 @@
 
 // Enums
 enum AppStatus { TERMINATED, RUNNING };
+enum Direction { LEFT, RIGHT };
 
 // Global Constants
-constexpr int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 450, FPS = 60;
+constexpr int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 450, FPS = 60, SIDES = 4;
+
+constexpr Vector2 ORIGIN = {SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0};
 
 // Global Variables
 AppStatus gAppStatus = RUNNING;
+float gAngle = 0.0f;
+float gScaleFactor = 100;
+Direction gDirection = LEFT;
+Vector2 gPosition = {0.0f, 0.0f};
 
 // Function Declarations
 void initialise();
@@ -34,12 +41,33 @@ void processInput() {
 }
 
 void update() {
+    // scaling
+    gScaleFactor *= 1.001;
+
+    // translation
+    if (gPosition.x < SCREEN_WIDTH) {
+        gPosition.x += 1.0f;
+    }
+    if (gPosition.y < SCREEN_HEIGHT) {
+        gPosition.y += 0.3f;
+    }
+
+    // rotation
+    gAngle += 1.0f * gDirection == RIGHT ? 1 : -1;
+    if (gAngle > 20.0f) {
+        gDirection = LEFT;
+    }
+    if (gAngle < -20.0f) {
+        gDirection = RIGHT;
+    }
 }
 
 void render() {
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
+
+    DrawPoly(gPosition, SIDES, gScaleFactor, gAngle, ORANGE);
 
     EndDrawing();
 }
