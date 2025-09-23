f99fc3089a92a80ba06f4b9169c4fccb89f1a4d8
Perry Huang <perryhuang2015@gmail.com>
2025-09-23 16:08:16 -0400

    feat: add group solution to main

--- 02-textures/main.cpp
+++ 02-textures/main.cpp
@@ -1,33 +1,34 @@
-#include <math.h>
-#include <raylib.h>
-
-#include <iostream>
-#include <vector>
-
 #include "CS3113/cs3113.h"
+#include <math.h>
 
 enum Member { MURDOC, TWO_D, RUSSEL, NOODLE };
-
+int quad = 0;
 // Global Constants
-constexpr int SCREEN_WIDTH = 1600 / 2, SCREEN_HEIGHT = 900 / 2, FPS = 60, SIZE = 200, FRAME_LIMIT = 100;
-constexpr float MAX_AMP = 10.0f;
+constexpr int   SCREEN_WIDTH  = 1600 / 2,
+                SCREEN_HEIGHT = 900 / 2,
+                FPS           = 60,
+                SIZE          = 200,
+                FRAME_LIMIT   = 100;
+constexpr float MAX_AMP       = 10.0f;
 
-constexpr char BG_COLOUR[] = "#000000";
-constexpr Vector2 ORIGIN = {SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0};
-constexpr Vector2 BASE_SIZE = {(float)SIZE, (float)SIZE};
+constexpr char    BG_COLOUR[] = "#000000";
+constexpr Vector2 ORIGIN      = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+constexpr Vector2 BASE_SIZE   = { (float) SIZE, (float) SIZE };
 
 // Image owned by Gorillaz @see https://gorillaz.com/
 constexpr char ALBUM_COVER_FP[] = "assets/demon_days.png";
 
 // Global Variables
-AppStatus gAppStatus = RUNNING;
-float gScaleFactor = SIZE, gAngle = 0.0f, gPulseTime = 0.0f;
-Vector2 gPosition = ORIGIN;
-Vector2 gScale = BASE_SIZE;
-float gPreviousTicks = 0.0f;
-float gPreviousTime = 0.0f;
-std::vector<std::vector<float>> gCoords;
-int gCurrMember = MURDOC;
+AppStatus gAppStatus     = RUNNING;
+float     gScaleFactor   = SIZE,
+          gAngle         = 0.0f,
+          gPulseTime     = 0.0f,
+          gPreviousTicks = 0.0f;
+Vector2   gPosition      = ORIGIN;
+Vector2   gScale         = BASE_SIZE;
+
+int gFrameCounter = 0;
+
 Texture2D gTexture;
 
 // Function Declarations
@@ -38,27 +39,26 @@ void render();
 void shutdown();
 
 // Function Definitions
-void initialise() {
+void initialise()
+{
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Textures");
 
     gTexture = LoadTexture(ALBUM_COVER_FP);
-    gCoords = {{0.0f, 0.0f, gTexture.width / 2.0f, gTexture.height / 2.0f},
-               {gTexture.width / 2.0f, 0.0f, gTexture.width / 2.0f, gTexture.height / 2.0f},
-               {0.0f, gTexture.height / 2.0f, gTexture.width / 2.0f, gTexture.height / 1.0f},
-               {gTexture.width / 2.0f, gTexture.height / 2.0f, gTexture.width / 1.0f, gTexture.height / 1.0f}};
 
     SetTargetFPS(FPS);
 }
 
-void processInput() {
+void processInput() 
+{
     if (WindowShouldClose()) gAppStatus = TERMINATED;
 }
 
-void update() {
+void update() 
+{
     /**
      * @todo Calculate delta time
      */
-    float ticks = (float)GetTime();
+    float ticks = (float) GetTime();
     float deltaTime = ticks - gPreviousTicks;
     gPreviousTicks = ticks;
 
@@ -67,26 +67,29 @@ void update() {
      */
     gPulseTime += 1.0f * deltaTime;
 
-    gScale = {BASE_SIZE.x + MAX_AMP * cos(gPulseTime), BASE_SIZE.y + MAX_AMP * cos(gPulseTime)};
+    gScale = {
+        BASE_SIZE.x + MAX_AMP * cos(gPulseTime),
+        BASE_SIZE.y + MAX_AMP * cos(gPulseTime)
+    };
 
     /**
      * @todo Switch member every 100 fames
+     * 
      */
-    if (ticks > gPreviousTime + 1.0f) {
-        gPreviousTime = ticks;
-        if (gCurrMember == MURDOC) {
-            gCurrMember = TWO_D;
-        } else if (gCurrMember == TWO_D) {
-            gCurrMember = RUSSEL;
-        } else if (gCurrMember == RUSSEL) {
-            gCurrMember = NOODLE;
-        } else if (gCurrMember == NOODLE) {
-            gCurrMember = MURDOC;
-        }
+    gFrameCounter++;
+    if(gFrameCounter < 100){
+        return;
     }
+    quad += 1;
+    gFrameCounter = 0;
+
+    // if(quad > 3){
+    //     quad = 0;
+    // }
 }
 
-void render() {
+void render()
+{
     BeginDrawing();
     ClearBackground(ColorFromHex(BG_COLOUR));
 
@@ -94,30 +97,84 @@ void render() {
      * @todo Design your UV coordinates (i.e. textureArea) so that only one
      * member is being rendered onto the screen.
      */
-    Rectangle textureArea = {// top-left corner
-                             gCoords[gCurrMember][0], gCoords[gCurrMember][1], static_cast<float>(gScale.x),
-                             static_cast<float>(gScale.y)};
+    
+    Rectangle picture;
+    switch(quad % 4){
+        case 0:
+            picture = {0.0f, 0.0f,
+            static_cast<float>(gTexture.width/2),
+            static_cast<float>(gTexture.height/2)};
+            break;
+        case 1:
+            picture = {static_cast<float>(gTexture.width/2), 0.0f,
+            static_cast<float>(gTexture.width/2),
+            static_cast<float>(gTexture.height/2)};
+            break;
+        case 2: //
+            picture = {static_cast<float>(gTexture.height/2),static_cast<float>(gTexture.width/2),
+            static_cast<float>(gTexture.width/2),
+            static_cast<float>(gTexture.height/2)};
+            break;
+        case 3: 
+            picture = {0.0f,static_cast<float>(gTexture.width/2),
+                static_cast<float>(gTexture.width/2),
+                static_cast<float>(gTexture.height/2)};
+                break;
+
+    }
+
+    // Rectangle textureArea = {
+    //     // top-left corner
+         
+    //     0.0f, 0.0f,
+
+    //     // bottom-right corner (of texture)
+    //     static_cast<float>(gTexture.width/2),
+    //     static_cast<float>(gTexture.height/2)
+        
+      
+    //    //static_cast<float>(gTexture.height)/2,  static_cast<float>(gTexture.width/2),
+    //     //static_cast<float>(gTexture.width/2), static_cast<float>(gTexture.height)
+        
+    // };
+
+
     // Destination rectangle – centred on gPosition
-    Rectangle destinationArea = {gCoords[gCurrMember][2], gCoords[gCurrMember][3], static_cast<float>(gScale.x),
-                                 static_cast<float>(gScale.y)};
+    Rectangle destinationArea = {
+        gPosition.x,
+        gPosition.y,
+        static_cast<float>(gScale.x),
+        static_cast<float>(gScale.y)
+    };
 
     // Origin inside the source texture (centre of the texture)
-    Vector2 objectOrigin = {static_cast<float>(gScale.x) / 2.0f, static_cast<float>(gScale.y) / 2.0f};
+    Vector2 objectOrigin = {
+        static_cast<float>(gScale.x) / 2.0f,
+        static_cast<float>(gScale.y) / 2.0f
+    };
 
     // Render the texture on screen
-    DrawTexturePro(gTexture, textureArea, destinationArea, objectOrigin, gAngle, WHITE);
+    DrawTexturePro(
+        gTexture, 
+        picture, 
+        destinationArea, 
+        objectOrigin, 
+        gAngle, 
+        WHITE
+    );
+    
 
     EndDrawing();
 }
 
-void shutdown() {
-    CloseWindow();
-}
+void shutdown() { CloseWindow(); }
 
-int main(void) {
+int main(void)
+{
     initialise();
 
-    while (gAppStatus == RUNNING) {
+    while (gAppStatus == RUNNING)
+    {
         processInput();
         update();
         render();
